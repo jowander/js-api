4c6d9c36b644bcf6425188c8b030530788dfbd01
jowander <74373353+jowander@users.noreply.github.com>
2022-02-16 13:47:16 +0100

    new

---
+++ task-3/js/details.js
@@ -0,0 +1,3 @@
+const select = document.querySelector("select");
+const createList = document.querySelector(".list");
+
--- task-3/js/script.js
+++ task-3/js/script.js
@@ -8,11 +8,38 @@
 
 const url ="https://breakingbadapi.com/api/characters/";
 
-async function getCharacters() {
-    const response = await fetch(url);
-    const jsonResult = await response.json();
+const queryString = document.location.search;
 
-    console.log(jsonResult);
+const params = new URLSearchParams(queryString);
+
+const id = params.get("id");
+console.log(queryString);
+const newDivs = document.querySelector(".container");
+
+async function getCharacters() {
+    try {
+        const response = await fetch(url);
+        const jsonResult = await response.json();
+
+        console.log(jsonResult);
+
+        const characterDetails = jsonResult[0];
+
+        console.log(characterDetails);
+
+        newDivs.innerHTML = "";
+
+        createHTML(characterDetails);
+    } catch(e) {
+
+    }
+    
 }
 
 getCharacters();
+
+
+
+function createHTML(characterDetails) {
+    newDivs.innerHTML = `<a href="details.html">${characterDetails.name}</a>`;
+}
